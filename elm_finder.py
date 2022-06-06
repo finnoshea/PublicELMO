413999c0bbbbd8aec28a6ee51d571b8ed5f18bc0
finnoshea <finn.h.oshea@gmail.com>
2022-06-06 15:39:39 -0700

    0.968 AUC with quantile=0.998 and bes_thresh=1.0

--- elm_finder.py
+++ elm_finder.py
@@ -12,7 +12,7 @@ def diff_signal(arr: np.array) -> np.array:
 
 def diff_fil(arr: np.array) -> np.array:
     """ ms-scale smoothing before diff """
-    scale = 100
+    scale = 1
     y = np.convolve(arr, np.ones(scale) / scale, mode='same')
     return diff_signal(y)
 
